346a265f85b0fe6dab3f52a9f9e3ae4cae8c0deb
Kei Wang <19126089@student.curtin.edu.au>
2018-10-12 11:57:52 +0800

    Begin classfication. First step cross-validation

--- assignment.py
+++ assignment.py
@@ -1,10 +1,16 @@
+"""	Author: Kei Sum Wang -id:19126089
+	Data mining COMP3009 assignment
+"""
+import numpy as np
 import weka.core.jvm as jvm
 import weka.core.converters as converters
 from weka.filters import Filter
 from weka.core.converters import Loader, Saver
-import numpy as np
+from weka.classifiers import Classifier, PredictionOutput, Evaluation
+from weka.core.classes import Random
 
 
+"""function to return index of attribute with significant num of missing values"""
 def mostMissing(data):
 	strang = ""
 	num_attr = data.num_attributes
@@ -23,14 +29,81 @@ def mostMissing(data):
 	
 	return strang
 
-def filters(data, fType, ops):
+"""function to return index of attributes that do not have many distinct values"""
+#distinct values make up only 1% of the attribute	
+def notDistinct(data):
+	strang = ""
+	num_attr = data.num_attributes
+	num_instances = data.num_instances
+	all_attribute_stats = data.attribute_stats
+	
+	for att in range(0,num_attr):
+		stats = all_attribute_stats(att)
+		attribute = data.attribute(att)
+		
+		pctNotDist = float(stats.distinct_count) / float(num_instances)
+		if pctNotDist < 0.01:
+			strang += str(attribute.index+1) + ','
 
-	filt = Filter(classname="weka.filters.unsupervised.attribute." + fType, options = ops)
+	strang = strang[:-1] #remove last comma from string
+	print(strang)
+	
+	return strang
+
+"""function handling unsupervised filters"""
+def unsupFilters(data, fType, ops):
+
+	filt = Filter(classname="weka.filters.unsupervised." + fType, options = ops)
 	filt.inputformat(data)     # let the filter know about the type of data to filter
 	filtered = filt.filter(data)   # filter the data
 	
 	return filtered 
 
+"""function handling supervised filters"""
+def supFilters(data, fType, ops):
+
+	filt = Filter(classname="weka.filters.supervised." + fType, options = ops)
+	filt.inputformat(data)     # let the filter know about the type of data to filter
+	filtered = filt.filter(data)   # filter the data
+	
+	return filtered
+	
+def naiveBayes(data):
+	
+	classifier = Classifier(classname="weka.classifiers.bayes.NaiveBayes")
+	nfolds=5
+	rnd = Random(0)
+	evaluation = Evaluation(data)
+	evaluation.crossvalidate_model(classifier, data,
+	nfolds, rnd)
+	print(" Cross-validation information")
+	print(evaluation.summary())
+	
+		
+def IBK(data):
+	
+	classifier = Classifier(classname="weka.classifiers.lazy.IBk")
+	nfolds=5
+	rnd = Random(0)
+	evaluation = Evaluation(data)
+	evaluation.crossvalidate_model(classifier, data,
+	nfolds, rnd)
+	print(" Cross-validation information")
+	print(evaluation.summary())
+	
+def treeJ48(data):
+	
+	classifier = Classifier(classname="weka.classifiers.trees.J48")
+	nfolds=5
+	rnd = Random(0)
+	evaluation = Evaluation(data)
+	evaluation.crossvalidate_model(classifier, data,
+	nfolds, rnd)
+	print(" Cross-validation information")
+	print(evaluation.summary())	
+
+
+"""function to prepare test and train"""	
 def preparation():
 	data_file="csvfiles/data.csv"
 
@@ -41,15 +114,31 @@ def preparation():
 		data.class_is_last()
 		
 		miss = mostMissing(data)#find attributes with significant missing data
-		data = filters(data, "Remove", ["-R", "1,"+ miss])#remove id and most mising value attributes
-		data = filters(data, "RemoveUseless", [])
-		data = filters(data, "NumericToNominal", ["-R", "last"])#class convert to nominal
-		data = filters(data, "ReplaceMissingValues", [])
-		data = filters(data, "Normalize", [])
-		print(data)
+		data = unsupFilters(data, "attribute.Remove", ["-R", "1,"+ miss])#remove id and most mising value attributes
+		data = unsupFilters(data, "attribute.RemoveUseless", [])#remove use less attributes
+		
+		nonDistinct = notDistinct(data)#find attributes that are not distinct and convert them to nominal
+		data = unsupFilters(data, "attribute.NumericToNominal", ["-R", "last," + nonDistinct])#class convert to nominal
+		data = unsupFilters(data, "attribute.ReplaceMissingValues", [])#replace missing values
+		data = unsupFilters(data, "attribute.Normalize", [])#normalize attributes to create less bias
+		
+		#split data into test and training set
+		test = unsupFilters(data, "instance.RemoveRange", ["-V", "-R", "901-1000"])
+		train = unsupFilters(data, "instance.RemoveRange", ["-R", "901-1000"])
+		train = supFilters(train, "instance.SMOTE", ["-P", "160.0"])
+		#print(data)
 		
 		saver = Saver(classname="weka.core.converters.ArffSaver")
-		saver.save_file(data, "test.arff")
+		saver.save_file(test, "test.arff")
+		saver.save_file(train, "train.arff")
+		
+		#Performing cross validation using naiveBayes, IBk and j48 tree
+		print("Naive Bayes")
+		naiveBayes(train)
+		print("IBK")
+		IBK(train)
+		print("J48 tree")
+		treeJ48(train)
 		
 		print("Data loaded successfully")
 	except IOError:
